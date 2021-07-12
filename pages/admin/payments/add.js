23dda2313d5ad2301de935691acffc97c07befec
iRaySpace <irayspacii@gmail.com>
2021-07-12 23:55:47 +0800

    feat(add): member last name

--- pages/admin/payments/add.js
+++ pages/admin/payments/add.js
@@ -1,4 +1,4 @@
-import { Heading, TextInputField, Button } from "evergreen-ui";
+import { Heading, Autocomplete, TextInputField, Button } from "evergreen-ui";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -10,13 +10,31 @@ export default function PaymentsAdd() {
 
   useEffect(async () => {
     const res = await axios.get("/api/members?fields=id,last_name");
-    setMembers(res.data);
+    setMembers(res.data.results);
   }, []);
+
+  const membersByLastName = members.map((member) => member["last_name"]);
 
   return (
     <div>
       <Heading size={900}>Add Payment</Heading>
-      <TextInputField required label="Last Name" placeholder="Doe" />
+      <Autocomplete
+        title="Member"
+        items={membersByLastName}
+        onChange={(value) => console.log(value)}
+      >
+        {(props) => {
+          const { getInputProps, getRef, inputValue } = props;
+          return (
+            <TextInputField
+              label="Member"
+              value={inputValue}
+              ref={getRef}
+              {...getInputProps()}
+            />
+          );
+        }}
+      </Autocomplete>
       <Button appearance="primary">Submit</Button>
     </div>
   );
